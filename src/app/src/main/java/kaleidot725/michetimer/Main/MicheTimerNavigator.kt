0c7bd2bd60f6e7380ea982b146307f49670b349b
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2018-07-29 22:07:50 +0900

    Add アラームをとりあえず鳴らせるようにした

--- src/app/src/main/java/kaleidot725/michetimer/Main/MicheTimerNavigator.kt
+++ src/app/src/main/java/kaleidot725/michetimer/Main/MicheTimerNavigator.kt
@@ -2,9 +2,11 @@ package kaleidot725.michetimer.main
 
 import android.support.v7.widget.PopupMenu
 import android.view.View
+import kaleidot725.michetimer.models.Timer
 
 interface MicheTimerNavigator {
-    fun onStartAlarmTimer(name : String)
+    fun onStartAlarmTimer(timer : Timer)
+    fun onStopAlarmTimer(timer : Timer)
     fun onStartEditTimer()
     fun onStartDeleteTimer(view : View,  menuListener : PopupMenu.OnMenuItemClickListener?)
 }
