62096ccf381b8fb78452c2624c944be3e4fdc378
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2018-09-11 00:36:30 +0900

    Add TimerRepository ファイル保存機能追加

--- src/app/src/main/java/kaleidot725/michetimer/Main/MicheTimerFragment.kt
+++ src/app/src/main/java/kaleidot725/michetimer/Main/MicheTimerFragment.kt
@@ -3,6 +3,7 @@ package kaleidot725.michetimer.main
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.databinding.Observable
+import android.databinding.ObservableArrayList
 import android.databinding.ObservableList
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -16,12 +17,12 @@ import kaleidot725.michetimer.models.ViewModelFactory
 import kaleidot725.michetimer.R
 import kaleidot725.michetimer.databinding.FragmentMicheTimerBinding
 import kaleidot725.michetimer.models.Timer
+import kaleidot725.michetimer.models.TimerRepository
 
 class MicheTimerFragment() : Fragment() {
+    private val timers :  ObservableList<Timer> = ObservableArrayList()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreate(savedInstanceState)
         return inflater.inflate(R.layout.fragment_miche_timer, container, false)
     }
@@ -39,12 +40,13 @@ class MicheTimerFragment() : Fragment() {
             adapter = TimerListAdapter(viewModel.timerViewModels)
         }
 
-        ViewModelFactory.timers?.addOnListChangedCallback(object : ObservableList.OnListChangedCallback<ObservableList<Timer>>(){
-
-
+        ViewModelFactory.timerRepository?.addOnListChangedCallback(object : ObservableList.OnListChangedCallback<ObservableList<Timer>>(){
             override fun onItemRangeInserted(sender: ObservableList<Timer>?, positionStart: Int, itemCount: Int) {
                 if (sender != null) {
-                    viewModel.timerViewModels.add(TimerViewModel(ViewModelFactory?.micheTimerNavigator as MicheTimerNavigator, sender.get(positionStart), sender))
+                    viewModel.timerViewModels.add(TimerViewModel(   ViewModelFactory?.micheTimerNavigator as MicheTimerNavigator,
+                                                                    ViewModelFactory?.timerRepository?.findAll()?.get(positionStart) as Timer,
+                                                                    ViewModelFactory?.timerRepository as TimerRepository))
+
                     recyclerView.adapter.notifyItemInserted(positionStart)
                 }
             }
