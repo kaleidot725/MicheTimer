353cea2dd323929dbd9bc3f958aa03bf97c25dea
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2018-12-12 23:01:14 +0900

    Migrate AndroidX

--- src/app/src/main/java/kaleidot725/michetimer/addtimer/AddTimerViewModel.kt
+++ src/app/src/main/java/kaleidot725/michetimer/addtimer/AddTimerViewModel.kt
@@ -1,7 +1,7 @@
 package kaleidot725.michetimer.addtimer
 
-import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
--- src/app/src/main/java/kaleidot725/michetimer/service/TimerRunnerService.kt
+++ src/app/src/main/java/kaleidot725/michetimer/service/TimerRunnerService.kt
@@ -8,8 +8,8 @@ import android.content.Intent
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
-import android.support.v4.app.NotificationCompat
-import android.support.v7.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import kaleidot725.michetimer.R
 import android.app.PendingIntent
