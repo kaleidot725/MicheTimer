9cd3feaae983fe3cf0071ef2507d7646d8be7430
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2018-12-22 19:32:24 +0900

    Add validation for addTimer
    *  Add Timerにてタイマーを追加するときのValidationを追加した

--- src/app/src/main/java/kaleidot725/michetimer/addtimer/AddTimerFragment.kt
+++ src/app/src/main/java/kaleidot725/michetimer/addtimer/AddTimerFragment.kt
@@ -32,7 +32,6 @@ class AddTimerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val viewModel = ViewModelProviders.of(this, AddTimerViewModelFactory).get(AddTimerViewModel::class.java)
-        val nameEdit = view.findViewById<EditText>(R.id.name_edittext)
 
         val secondSpinner = view.findViewById<Spinner>(R.id.second_spinner)
         val secondAdapter = ArrayAdapter.createFromResource(view.context ,R.array.seconds, android.R.layout.simple_spinner_dropdown_item)
@@ -57,6 +56,7 @@ class AddTimerFragment : Fragment() {
 
         val binding = DataBindingUtil.bind<FragmentAddTimerBinding>(view)
         binding?.setVariable(BR.viewmodel, viewModel)
+        binding?.setLifecycleOwner(this)
     }
 
     private object AddTimerViewModelFactory : ViewModelProvider.Factory{
